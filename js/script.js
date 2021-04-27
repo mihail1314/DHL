9e554c3fe47d1ef016e8b752d6d6a5fcd1b50c82
Mihail <tasre51186@gmail.com>
2021-04-27 07:30:04 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-$('ul.inform__elem_wrap').on('click', 'li:not(.inform__elem_title_active)', function() {
-    $(this)
-        .addClass('inform__elem_title_active').siblings().removeClass('inform__elem_title_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+$(document).ready(function() {
+    $('ul.header__wrap_menu').on('click', 'li:not(.header__wrap-item_active)', function() {
+        $(this)
+            .addClass('header__wrap-item_active').siblings().removeClass('header__wrap-item_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 });
